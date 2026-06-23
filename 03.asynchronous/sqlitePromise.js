33582702948bef37e403b8da41acc948c881f3dc
samovar <samovar1214@gmail.com>
2026-06-23 21:14:53 +0900

    node-sqlite3の関数をPromiseベースにして定義

---
+++ 03.asynchronous/sqlitePromise.js
@@ -0,0 +1,17 @@
+export const run = (db, sql, params = []) => {
+  return new Promise((resolve, reject) => {
+    db.run(sql, params, function (err) {
+      if (err) reject(err);
+      else resolve(this);
+    });
+  });
+};
+
+export const get = (db, sql, params = []) => {
+  return new Promise((resolve, reject) => {
+    db.get(sql, params, function (err, row) {
+      if (err) reject(err);
+      else resolve(row);
+    });
+  });
+};
