22ce8aa28eea1b70ed9595157faa19ab7e70482f
samovar <samovar1214@gmail.com>
2026-06-23 21:10:44 +0900

    コールバックの形式で実装

---
+++ 03.asynchronous/callback.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+import sqlite3 from "sqlite3";
+import timers from "timers/promises";
+
+function normalFlow(db) {
+  db.run(
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+    () => {
+      db.run("INSERT INTO books (title) VALUES (?)", ["異邦人"], function () {
+        console.log(`id: ${this.lastID}が追加されました`);
+
+        db.get(
+          "SELECT * FROM books WHERE id = ?",
+          [this.lastID],
+          (err, row) => {
+            console.log(row);
+
+            db.run("DROP TABLE books", () => {});
+          },
+        );
+      });
+    },
+  );
+}
+
+function errorFlow(db) {
+  db.run(
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+    () => {
+      db.run("INSERT INTO books (title) VALUES (?)", ["異邦人"], () => {
+        db.run(
+          "INSERT INTO books (title) VALUES (?)",
+          ["異邦人"],
+          function (err) {
+            if (err) console.error(err.message);
+
+            db.get("SELECT hoge FROM books", (err) => {
+              if (err) console.error(err.message);
+
+              db.run("DROP TABLE books", () => {});
+            });
+          },
+        );
+      });
+    },
+  );
+}
+
+const db = new sqlite3.Database(":memory:");
+
+console.log("エラーなしのプログラム");
+normalFlow(db);
+
+await timers.setTimeout(100);
+
+console.log("エラーありのプログラム");
+errorFlow(db);
