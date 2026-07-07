45111406f8b493a294b23205afd34f9d1c54f7a3
samovar <samovar1214@gmail.com>
2026-07-07 21:11:40 +0900

    if文はブロックを使うように修正しgetをアロー関数に変更

--- 03.asynchronous/sqlitePromise.js
+++ 03.asynchronous/sqlitePromise.js
@@ -1,17 +1,23 @@
 export const run = (db, sql, params) => {
   return new Promise((resolve, reject) => {
     db.run(sql, params, function (err) {
-      if (err) reject(err);
-      else resolve(this);
+      if (err) {
+        reject(err);
+      } else {
+        resolve(this);
+      }
     });
   });
 };
 
 export const get = (db, sql, params) => {
   return new Promise((resolve, reject) => {
-    db.get(sql, params, function (err, row) {
-      if (err) reject(err);
-      else resolve(row);
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row);
+      }
     });
   });
 };
