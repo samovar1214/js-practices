5015baf6c3cb7e6b41cf29528f2ec283d5349991
samovar <samovar1214@gmail.com>
2026-07-21 21:28:15 +0900

    変数名をクラス名に合わせて、変数の寿命を短く修正

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -4,13 +4,13 @@ import MemoController from "./lib/memo_controller.js";
 import MemoDatabase from "./lib/memo_database.js";
 import ConsoleView from "./lib/console_view.js";
 
-const option = process.argv.slice(2)[0];
 const database = new MemoDatabase("./memo.db");
 const view = new ConsoleView();
 
-const app = new MemoController({
+const controller = new MemoController({
   database,
   view,
 });
 
-app.run(option);
+const option = process.argv.slice(2)[0];
+controller.run(option);
